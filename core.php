ce4e02acdf3e464a1887acc27728b0e7214116a3
ldfavale <ldfavale@gmail.com>
2016-10-28 16:35:05 -0300

    probando

--- core.php
+++ core.php
@@ -9,11 +9,11 @@ $base = new database();
 define('DB_HOST',$base->getHost());
 define('DB_USER',$base->getUser());
 define('DB_PASS',$base->getPass());
-define('DB_NAME',$base->getDB());
+define('DB_NAME',$base->getDB()); 
 
-//define('APP_URL',"http://".gethostname()."/");
 define('APP_URL',"http://contralor.cure.edu.uy/");
 define('ABS_PATH',getcwd());
 define('APP_NAME',"SGPH");
 define('APP_DIR',$_SERVER["DOCUMENT_ROOT"]."/".APP_NAME);
+
 ?>
